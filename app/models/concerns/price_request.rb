6bb6e9e76f188dc930e26b248a301ffb0ec253d7
Piyush <psdev52@gmail.com>
2017-12-05 12:34:17 +0530

    Add region column and apply validation on price_request model

--- app/models/concerns/price_request.rb
+++ app/models/concerns/price_request.rb
@@ -2,5 +2,7 @@ class PriceRequest
   include ActiveModel::Model
   include ActiveModel::Validations::Callbacks
 
-  attr_accessor :address, :avg_long_term_rental
+  attr_accessor :address, :avg_long_term_rental, :region
+  validates_presence_of :address
+  validates_presence_of :avg_long_term_rental
 end
